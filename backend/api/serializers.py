02ac008cb0bd5a81a84aad98dcb750e1b276fd8c
Liveweek <konakov.po@yandex.ru>
2022-01-08 16:44:34 +0300

    some fix and comments

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,11 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+        Сериализатор катомного токена, для которого было
+        добавлено поле имени пользователя (username)
+    """
+    
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
